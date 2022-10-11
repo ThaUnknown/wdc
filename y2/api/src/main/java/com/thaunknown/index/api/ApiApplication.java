c3f69508e161b9cf4026b59c5a114cd1220183a6
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-10-11 14:31:37 +0200

    fix: better database, better path resolving

--- y2/api/src/main/java/com/thaunknown/index/api/ApiApplication.java
+++ y2/api/src/main/java/com/thaunknown/index/api/ApiApplication.java
@@ -1,57 +1,42 @@
 package com.thaunknown.index.api;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ResourceUtils;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 @RestController
 @SpringBootApplication
 public class ApiApplication {
-  static JsonElement json;
   static Gson gson = new Gson();
 
-  public static void main(String[] args) throws FileNotFoundException, IOException {
-    String path = "D:/Webdevelopment/spring/api/src/main/resources/export.json";
-    BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-
-    json = gson.fromJson(bufferedReader, JsonElement.class);
-    bufferedReader.close();
+  public static void main(String[] args) {
     SpringApplication.run(ApiApplication.class, args);
   }
 
   @GetMapping("/")
   String root() {
-    return gson.toJson(json);
+    return "No path specified. Try: collections, columns, items, sections. \n Eg: /columns or /items/{slug}";
   }
 
   @GetMapping("/{type}")
-  String collectionType(@PathVariable String type) {
-    return gson.toJson(json.getAsJsonObject().get(type));
+  String collectionType(@PathVariable String type) throws FileNotFoundException, IOException {
+    return Files.readString(ResourceUtils.getFile("classpath:" + type + ".json").toPath());
   }
 
   @GetMapping("/{type}/{id}")
-  String collectionItem(@PathVariable String type, @PathVariable String id) {
-    JsonArray collectionType = json.getAsJsonObject().get(type + 's').getAsJsonArray();
-    return gson.toJson(getById(collectionType, id));
-  }
-
-  JsonElement getById(JsonArray items, String id) {
-    for (JsonElement item : items) {
-      if (item.getAsJsonObject().get("_id").getAsString().equals(id)) {
-        return item;
-      }
-    }
-    return null;
+  String collectionItem(@PathVariable String type, @PathVariable String id) throws FileNotFoundException, IOException {
+    JsonElement json = gson.fromJson(Files.readString(ResourceUtils.getFile("classpath:" + type + ".json").toPath()), JsonElement.class);
+    JsonElement element = json.getAsJsonObject().get(id);
+    return gson.toJson(element);
   }
 }
--- y2/api/src/main/java/com/thaunknown/index/api/MyErrorController.java
+++ y2/api/src/main/java/com/thaunknown/index/api/MyErrorController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController  {
-
     @RequestMapping("/error")
     public String error() {
         return "Invalid Path";
+    }
+
+    public String getErrorPath() {
+        return "/error";
     }
 }
